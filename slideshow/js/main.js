c1fdacedc4d95567c27a8b56b7e548b6e1a27bda
melank <im0004vr@gmail.com>
2019-05-29 00:53:47 +0900

    finish

--- slideshow/js/main.js
+++ slideshow/js/main.js
@@ -2,15 +2,93 @@
   'use strict';
 
   const images = [
-    'img/pic00.png',
-    'img/pic01.png',
-    'img/pic02.png',
-    'img/pic03.png',
-    'img/pic04.png',
-    'img/pic05.png',
-    'img/pic06.png',
-    'img/pic07.png',
+    'imgs/pic00.png',
+    'imgs/pic01.png',
+    'imgs/pic02.png',
+    'imgs/pic03.png',
+    'imgs/pic04.png',
+    'imgs/pic05.png',
+    'imgs/pic06.png',
+    'imgs/pic07.png',
   ];
 
   let currentNum = 0;
+  let timerId = 0;
+
+  function setMainImage(image) {
+    document.querySelector('main img').src = image;
+  }
+
+  function addCurrentClass() {
+    document.querySelectorAll('.thumbnails li')[currentNum].classList.add('current');
+  }
+
+  function removeCurrentClass() {
+    document.querySelectorAll('.thumbnails li')[currentNum].classList.remove('current');
+  }
+
+  setMainImage(images[currentNum]);
+
+  const thumbnails = document.querySelector('.thumbnails');
+  images.forEach((image, index) => {
+    const li = document.createElement('li');
+    if (index === currentNum) {
+      li.classList.add('current');
+    }
+
+    li.addEventListener('click', () => {
+      setMainImage(image);
+      removeCurrentClass();
+      currentNum = index;
+      addCurrentClass();
+    });
+
+    const img = document.createElement('img');
+    img.src = image;
+    li.appendChild(img);
+    thumbnails.appendChild(li);
+  });
+
+  const next = document.getElementById('next');
+  next.addEventListener('click', () => {
+    removeCurrentClass();
+    currentNum++;
+    if(currentNum === images.length) {
+      currentNum = 0;
+    }
+    addCurrentClass();
+    setMainImage(images[currentNum]);
+  });
+
+  const prev = document.getElementById('prev');
+  prev.addEventListener('click', () => {
+    removeCurrentClass();
+    currentNum--;
+    if(currentNum === -1) {
+      currentNum = images.length - 1;
+    }
+    addCurrentClass();
+    setMainImage(images[currentNum]);
+  });
+
+  function playSlideShow() {
+    timerId = setTimeout(() => {
+      next.click();
+      playSlideShow();
+    }, 100);
+  }
+
+  const play = document.getElementById('play');
+  const pause = document.getElementById('pause');
+  play.addEventListener('click', () => {
+    play.classList.add('hidden');
+    pause.classList.remove('hidden');
+    playSlideShow();
+  });
+  pause.addEventListener('click', () => {
+    pause.classList.add('hidden');
+    play.classList.remove('hidden');
+    clearTimeout(timerId);
+  });
+
 })();
