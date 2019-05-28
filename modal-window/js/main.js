3a7fdb1ae4a9c1c2c99e84bd2d5535c37377e10f
melank <im0004vr@gmail.com>
2019-05-29 01:48:55 +0900

    finish

---
+++ modal-window/js/main.js
@@ -0,0 +1,19 @@
+(function() {
+  'use strict';
+
+  const open = document.getElementById('open');
+  const close = document.getElementById('close');
+  const mask = document.getElementById('mask');
+  const modal = document.getElementById('modal');
+  open.addEventListener('click', function() {
+    modal.classList.remove('hidden');
+    mask.classList.remove('hidden');
+  });
+  close.addEventListener('click', function() {
+    modal.classList.add('hidden');
+    mask.classList.add('hidden');
+  });
+  mask.addEventListener('click', function() {
+    close.click();
+  });
+})();
