5422aeb333860c963d26c5d1c1bca178269015f8
melank <im0004vr@gmail.com>
2019-05-28 23:58:42 +0900

    update js

---
+++ slideshow/js/main.js
@@ -0,0 +1,16 @@
+(function() {
+  'use strict';
+
+  const images = [
+    'img/pic00.png',
+    'img/pic01.png',
+    'img/pic02.png',
+    'img/pic03.png',
+    'img/pic04.png',
+    'img/pic05.png',
+    'img/pic06.png',
+    'img/pic07.png',
+  ];
+
+  let currentNum = 0;
+})();
