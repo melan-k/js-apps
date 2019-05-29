f6d9f36742dfe02907cad8b52de7a52631214ed1
melank <masaki.vr@gmail.com>
2019-05-29 17:42:52 +0900

    use promise

--- js/main.js
+++ js/main.js
@@ -1,51 +1,62 @@
 function getUserInfo(userId) {
-  const request = new XMLHttpRequest();
-  request.open('GET', `https://api.github.com/users/${userId}`);
-  request.addEventListener("load", (event) => {
-    if (event.target.status != 200) {
-      console.error(`${event.target.status}: ${event.target.statusText}`);
-      return;  
-    }
-    // console.log(event.target.status);
-    // console.log(event.target.responseText);
-    const userInfo = JSON.parse(event.target.responseText);
+	return new Promise((resolve, reject) => {
+		const request = new XMLHttpRequest();
+		request.open('GET', `https://api.github.com/users/${userId}`);
+		request.addEventListener("load", (event) => {
+			if (event.target.status != 200) {
+				console.error(`${event.target.status}: ${event.target.statusText}`);
+				return;
+			}
+			// console.log(event.target.status);
+			// console.log(event.target.responseText);
+			const userInfo = JSON.parse(event.target.responseText);
+			const view = createView(userInfo);
 
-    const view = escapeHTML`
-      <h4>${userInfo.name} (@${userInfo.login})</h4>
-      <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
-      <dl>
-          <dt>Location</dt>
-          <dd>${userInfo.location}</dd>
-          <dt>Repositories</dt>
-          <dd>${userInfo.public_repos}</dd>
-      </dl>
-      `;
+			displayView(view);
+			resolve(); // 完了
+		});
+		request.addEventListener("error", () => {
+			console.error("Network Error!");
+			reject();
+		});
+		request.send();
+	});
+}
 
-      const result = document.getElementById("result");
-      result.innerHTML = view;
-  });
-  request.addEventListener("error", () => {
-    console.error("Network Error!");
-  });
-  request.send();
+function createView(userInfo) {
+    return escapeHTML`
+        <h4>${userInfo.name} (@${userInfo.login})</h4>
+        <img src="${userInfo.avatar_url}" alt="${userInfo.login}" height="100">
+        <dl>
+            <dt>Location</dt>
+            <dd>${userInfo.location}</dd>
+            <dt>Repositories</dt>
+            <dd>${userInfo.public_repos}</dd>
+        </dl>
+        `;
+}
+
+function displayView(view) {
+	const result = document.getElementById("result");
+	result.innerHTML = view;
 }
 
 function escapeSpecialChars(str) {
-  return str
-      .replace(/&/g, "&amp;")
-      .replace(/</g, "&lt;")
-      .replace(/>/g, "&gt;")
-      .replace(/"/g, "&quot;")
-      .replace(/'/g, "&#039;")
+	return str
+		.replace(/&/g, "&amp;")
+		.replace(/</g, "&lt;")
+		.replace(/>/g, "&gt;")
+		.replace(/"/g, "&quot;")
+		.replace(/'/g, "&#039;")
 }
 
 function escapeHTML(strings, ...values) {
-  return strings.reduce((result, string, i) => {
-    const value = values[i - 1];
-    if (typeof value === 'string') {
-      return result + escapeSpecialChars(value) + string;
-    } else {
-      return result + String(value) + string;
-    }
-  });
+	return strings.reduce((result, string, i) => {
+		const value = values[i - 1];
+		if (typeof value === 'string') {
+			return result + escapeSpecialChars(value) + string;
+		} else {
+			return result + String(value) + string;
+		}
+	});
 }
