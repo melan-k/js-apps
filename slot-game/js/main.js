6c4ba123b68870d50c2374c219b86ceed8fc71c1
melank <masaki.vr@gmail.com>
2019-05-30 23:32:58 +0900

    update js

--- slot-game/js/main.js
+++ slot-game/js/main.js
@@ -1,3 +1,28 @@
 (function() {
     'use strict';
+
+    class Panel {
+        constructor() {
+            const section = document.createElement('section');
+            section.classList.add('panel');
+
+            this.img = document.createElement('img');
+            this.img.src = 'img/seven.png';
+
+            this.stop = document.createElement('div');
+            this.stop.textContent = 'STOP';
+            this.stop.classList.add('stop');
+
+            section.appendChild(this.img);
+            section.appendChild(this.stop);
+
+            const main = document.querySelector('main');
+            main.appendChild(section);
+        }
+    }
+    const panes = [
+      new Panel(),
+      new Panel(),
+      new Panel(),  
+    ];
 })();
