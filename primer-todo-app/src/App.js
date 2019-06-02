1387e4dbad76e4c078bdf77145ef4c319d5aea17
melank <masaki.vr@gmail.com>
2019-06-02 18:36:54 +0900

    update events

--- primer-todo-app/src/App.js
+++ primer-todo-app/src/App.js
@@ -1,22 +1,40 @@
-import { element } from "./view/html-util.js";
+import { TodoListModel } from "./Model/TodoListModel.js";
+import { TodoItemModel } from "./Model/TodoItemModel.js";
+import { element, render } from "./view/html-util.js";
 
 export class App {
+    constructor() {
+        // TodoListの初期化
+        this.todoListModel = new TodoListModel();
+    }
     mount() {
         const formElement = document.querySelector("#js-form");
         const inputElement = document.querySelector("#js-form-input");
         const containerElement = document.querySelector("#js-todo-list");
         const todoItemCountElement = document.querySelector("#js-todo-count");
 
-        let todoItemCount = 0;
+        // 2. TodoListModelの状態が更新されたら表示を更新する
+        this.todoListModel.onChange(() => {
+            // TodoリストをまとめるList要素
+            const todoListElement = element`<ul />`;
+            // それぞれのTodoItem要素をTodoListElement以下へ追加する
+            const todoItems = this.todoListModel.getTodoItems();
+            todoItems.forEach(item => {
+                const todoItemElement = element`<li>${item.title}</li>`;
+                todoListElement.appendChild(todoItemElement);
+            });
+            // containerElementの中身をtodoListElementで上書きする
+            render(todoListElement, containerElement);
+            // アイテム数の表示を更新
+            todoItemCountElement.textContent = `Todoアイテム数: ${this.todoListModel.getTotalCount()}`;
+        });
 
         formElement.addEventListener("submit", (event) => {
             event.preventDefault();
-            const todoItemElement = element`<li>${inputElement.value}</li>`;
-            containerElement.appendChild(todoItemElement);
-
-            todoItemCount += 1;
-            todoItemCountElement.textContent = `Todoアイテム数: ${todoItemCount}`;
-
+            this.todoListModel.addTodo(new TodoItemModel({
+                title: inputElement.value,
+                completed: false
+            }));
             // 入力欄を空文字にリセットする
             inputElement.value = "";
         });
