8f89371faa1a003353c723a5716695a2f7483e4a
melank <masaki.vr@gmail.com>
2019-06-06 23:31:55 +0900

    add checkbox

--- primer-todo-app/src/App.js
+++ primer-todo-app/src/App.js
@@ -20,7 +20,17 @@ export class App {
             // それぞれのTodoItem要素をTodoListElement以下へ追加する
             const todoItems = this.todoListModel.getTodoItems();
             todoItems.forEach(item => {
-                const todoItemElement = element`<li>${item.title}</li>`;
+                // 完了済みなら checked 属性を付けて，未完了なら checked 属性を外す
+                const todoItemElement = item.completed
+                ? element`<li><input type="checkbox" class="checkbox" checked><s>${item.title}</s></input></li>`
+                : element`<li><input type="checkbox" class="checkbox">${item.title}</input></li>`
+                const inputCheckboxElement = todoItemElement.querySelector(".checkbox");
+                inputCheckboxElement.addEventListener("change", () => {
+                    this.todoListModel.updateTodo({
+                        id: item.id,
+                        completed: !item.completed
+                    });
+                });
                 todoListElement.appendChild(todoItemElement);
             });
             // containerElementの中身をtodoListElementで上書きする
