1558b4c7e52ae495658776959a26ccf432a63554
melank <masaki.vr@gmail.com>
2019-06-24 23:58:19 +0900

    complete

--- vue-sample/vue-todo/main.js
+++ vue-sample/vue-todo/main.js
@@ -26,6 +26,37 @@ const app = new Vue({
 		// 初期値を「-1」つまり「すべて」にする
 		current: -1
 	},
+	computed: {
+		computedTodos: function() {
+			// データ current が -1 ならすべて
+			// それ以外なら current と state が一致するものだけに絞り込む
+			return this.todos.filter(function(el) {
+				return this.current < 0 ? true : this.current === el.state
+			}, this)
+		},
+		labels() {
+			return this.options.reduce(function(a, b) {
+				return Object.assign(a, { [b.value]: b.label })
+			}, {})
+			// キーから見つけやすいように，次のように加工したデータを作成
+			// {0: '作業中', 1: '完了', -1: 'すべて'}
+		}
+	},
+	watch: {
+		// オプションを使う場合はオブジェクト形式にする
+		todos: {
+			// 引数はウォッチしているプロパティの変更後の値
+			handler: function(todos) {
+				todoStorage.save(todos)
+			},
+			// deepオプションでネストしているデータも監視できる
+			deep: true
+		}
+	},
+	created() {
+		// インスタンス作成時に自動的に fetch() する
+		this.todos = todoStorage.fetch()
+	},
 	methods: {
 		doAdd: function(event, value) {
 			// ref で名前をつけておいた要素を参照
@@ -46,42 +77,11 @@ const app = new Vue({
 			comment.value = ''
 		},
 		doChangeState: function(item) {
-			item.state = item.state ? 0 : 1
+			item.state = !item.state ? 1 : 0
 		},
 		doRemove: function(item) {
 			var index = this.todos.indexOf(item)
 			this.todos.splice(index, 1)
-		}
-	},
-	watch: {
-		// オプションを使う場合はオブジェクト形式にする
-		todos: {
-			// 引数はウォッチしているプロパティの変更後の値
-			handler: function(todos) {
-				todoStorage.save(todos)
-			},
-			// deepオプションでネストしているデータも監視できる
-			deep: true
-		}
-	},
-	created() {
-		// インスタンス作成時に自動的に fetch() する
-		this.todos = todoStorage.fetch()
-	},
-	computed: {
-		computedTodos: function() {
-			// データ current が -1 ならすべて
-			// それ以外なら current と state が一致するものだけに絞り込む
-			return this.todos.filter(function(el) {
-				return this.current < 0 ? true : this.current === el.state
-			}, this)
-		},
-		labels() {
-			return this.options.reduce(function(a, b) {
-				return Object.assign(a, { [b.value]: b.label })
-			}, {})
-			// キーから見つけやすいように，次のように加工したデータを作成
-			// {0: '作業中', 1: '完了', -1: 'すべて'}
 		}
 	}
 })
