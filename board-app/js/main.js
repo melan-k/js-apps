95a29bfa9a98e6ffdb24f25b210b2a7247662f28
melank <=>
2019-06-27 00:43:10 +0900

    update board-app

--- board-app/js/main.js
+++ board-app/js/main.js
@@ -1,10 +1,10 @@
 Vue.component('board-list', {
-	template: '<li>{{ name }} {{ date }}</br> {{ message }}</li>',
-	props: ['name', 'message', 'date']
+	template: '<li class="board-list"><div class="board-list__upper">名前：{{ name }}{{ date }}</div>{{ message }}</li>',
+	props: ['name', 'message', 'date', 'id'],
 })
 
 Vue.component('board-form', {
-	template: '<div class="form-area">名前: <input v-model:"name"> </br>コメント:\
+	template: '<div class="form-area">名前: <input v-model="name"> </br>コメント: \
 	<textarea v-model="message"></textarea> </br><button @click="doAdd">書き込む</button></div>',
 	data: function() {
 		return {
@@ -15,31 +15,35 @@ Vue.component('board-form', {
 	methods: {
 		doAdd: function() {
 			this.$emit('input', this.name, this.message)
+			this.message = ''
+			this.name = ''
 		}
 	}
 })
 
 var board = new Vue({
-	el: 'board',
+	el: '#board',
 	data: {
-		name: '',
-		message: '',
-		date: '',
 		lists: [
-
 		]
+	},
+	created: function(){
+		var vue = this;
+		firebase.database().ref('board').on('value', function(snapshot) {
+			vue.lists = snapshot.val();
+		});
 	},
 	methods: {
 		doAdd: function(name, message) {
 			var now = new Date();
-			this.lists.push({
+			firebase.database().ref('board').push({
 				name: name,
 				message: message,
 				date: now.getMonth() + 1 + '月' + 
 							now.getDate() + '日' + 
 							now.getHours() + '時' + 
 							now.getMinutes() + '分'
-			})
+			});
 		}
 	}
 })
