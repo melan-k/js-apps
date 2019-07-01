6bf3dcfd76ff748f507c20eda47daffc9f0193d0
melank <masaki.vr@gmail.com>
2019-07-01 17:16:28 +0900

    temporal commit]

--- node-begginers/server.js
+++ node-begginers/server.js
@@ -4,10 +4,20 @@ let url = require("url");
 function start(route, handle) {
   const port = 8000;
   function onRequest(request, response) {
+    let postData = "";
     const pathname = url.parse(request.url).pathname;
     if (!pathname.match(/favicon.ico/)) {
       console.log("\nRequest for " + pathname + " received.");
-      route(handle, pathname, response);
+      request.setEncoding('utf8');
+
+      request.addListener("data", function(postDataChunk) {
+        postData += postDataChunk;
+        console.log("Received POST data chunk '" + postDataChunk + "'.");
+      });
+
+      request.addListener("end", function() {
+        route(handle, pathname, response, postData);
+      });
     }
   }
   http.createServer(onRequest).listen(port);
