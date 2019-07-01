df005a4bcac953b1e447064e814657173def830c
melank <masaki.vr@gmail.com>
2019-07-01 23:29:46 +0900

    complete implemention

--- node-begginers/requestHandlers.js
+++ node-begginers/requestHandlers.js
@@ -1,4 +1,6 @@
-const exec = require('child_process').exec;
+let querystring = require("querystring");
+let fs = require("fs");
+let formidable = require("formidable");
 
 function start(response) {
   console.log("Request handler 'start' was called");
@@ -9,25 +11,54 @@ function start(response) {
       <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
       </head>
       <body>
-        <form action="/upload" method="post">
-          <textarea name="text" rows="20" cols="60"></textarea>
-          <input type="submit" value="Submit text" />
+        <form action="/upload" enctype="multipart/form-data" method="post">
+          <input type="file" name="upload">
+          <input type="submit" value="Upload file" />
         </form>
       </body>
     </html>
   `
-
   response.writeHead(200, {"Content-Type": "text/html"});
   response.write(body);
   response.end();
 }
 
-function upload(response) {
+function upload(response, request) {
   console.log("Request handler 'upload' was called");
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello Upload");
-  response.end();
+
+  const form = new formidable.IncomingForm();
+  console.log("about to parse");
+  form.parse(request, function(error, fields, files) {
+    console.log("parsing done");
+
+    fs.rename(files.upload.path, "tmp/test.png", function(err) {
+      if (err) {
+        fs.unlink("tmp/test.png");
+        fs.rename(files.upload.path, "tmp/test.png");
+      }
+    });
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write("received image;<br/>");
+    response.write("<img src='/show' />");
+    response.end();
+    });
+  }
+
+function show(response) {
+  console.log("Request handler 'show' was called.");
+  fs.readFile("tmp/test.png", "binary", function(error, file) {
+    if (error) {
+      response.writeHead(500, {"Content-type": "text/plain"});
+      response.write(error + "¥n");
+      response.end();
+    } else {
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(file, "binary");
+      response.end();
+    }
+  });
 }
 
 exports.start = start;
 exports.upload = upload;
+exports.show = show;
--- node-begginers/server.js
+++ node-begginers/server.js
@@ -2,26 +2,13 @@ let http = require("http");
 let url = require("url");
 
 function start(route, handle) {
-  const port = 8000;
   function onRequest(request, response) {
-    let postData = "";
     const pathname = url.parse(request.url).pathname;
-    if (!pathname.match(/favicon.ico/)) {
-      console.log("\nRequest for " + pathname + " received.");
-      request.setEncoding('utf8');
-
-      request.addListener("data", function(postDataChunk) {
-        postData += postDataChunk;
-        console.log("Received POST data chunk '" + postDataChunk + "'.");
-      });
-
-      request.addListener("end", function() {
-        route(handle, pathname, response, postData);
-      });
-    }
+    console.log("Request for " + pathname + " received.");
+    route(handle, pathname, response, request);
   }
-  http.createServer(onRequest).listen(port);
-  console.log('listening on port: ' + port);
+  http.createServer(onRequest).listen(8000);
+  console.log("Server has started");
 }
 
 exports.start = start;
